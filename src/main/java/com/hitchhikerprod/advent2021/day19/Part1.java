d78e29007955060cfad77de9e4365ee4a4ba49bb
Ben Cordes <cordes.ben@gmail.com>
2021-12-30 20:51:19 -0500

    day19 solution

--- src/main/java/com/hitchhikerprod/advent2021/day19/Part1.java
+++ src/main/java/com/hitchhikerprod/advent2021/day19/Part1.java
@@ -1,17 +1,15 @@
 package com.hitchhikerprod.advent2021.day19;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Part1 {
+    private static final boolean PART1 = false;
+
     private final DataProvider provider;
 
     public Part1(DataProvider provider) {
@@ -21,103 +19,15 @@ public class Part1 {
     public static void main(String[] argv) {
         final String inputFile = "/inputs/day19.txt";
         final DataProvider provider = DataProvider.from(inputFile);
-        System.out.println(new Part1(provider).oldSolve());
+        System.out.println(new Part1(provider).solve());
     }
-
-    private record Foo(int id, int orientation) {}
-    private record Bar(Foo f, Point delta) {}
 
     public long solve() {
-        Set<Scanner> scanners = IntStream.range(0, provider.getReadouts().size())
-                .mapToObj(id -> new Scanner(id, provider.getReadouts().get(id)))
-                .collect(Collectors.toSet());
-
-        Map<Foo, List<Bar>> scannerMap = new HashMap<>();
-
-        for (Scanner a : scanners) {
-            System.out.print(a.id() + ":");
-            for (Scanner b : scanners) {
-                if (b.id() <= a.id()) {
-                    continue;
-                }
-                System.out.print(" " + b.id());
-                for (int i = 0; i < Point.TRANSFORMATIONS.size(); i++) {
-                    List<Point> at = a.absoluteBeacons(i);
-                    for (int j = 0; j < Point.TRANSFORMATIONS.size(); j++) {
-                        List<Point> bt = b.absoluteBeacons(j);
-
-                        final OverlapDetector detector = new OverlapDetector(at, bt);
-                        if (detector.detect()) {
-                            var aReference = new Foo(a.id(), i);
-                            var bReference = new Foo(b.id(), j);
-                            scannerMap.computeIfAbsent(aReference, k -> new ArrayList<>());
-                            scannerMap.get(aReference).add(new Bar(bReference, detector.getDelta()));
-                            scannerMap.computeIfAbsent(bReference, k -> new ArrayList<>());
-                            scannerMap.get(bReference).add(new Bar(aReference, detector.getDelta().invert()));
-                        }
-                    }
-                }
-            }
-            System.out.println();
-        }
-
-        List<Bar> locations = null;
-        for (var f : scannerMap.keySet()) {
-            var candidate = new Bar(f, new Point(0,0,0));
-            Optional<List<Bar>> solution = recursiveBuild(scannerMap, scanners.size(), List.of(candidate));
-            if (solution.isPresent()) {
-                locations = solution.get();
-                break;
-            }
-        }
-
-        Set<Point> beacons = new HashSet<>();
-        for (var location : locations) {
-            final Scanner scanner = scanners.stream()
-                    .filter(s -> s.id() == location.f().id())
-                    .findFirst().orElseThrow();
-            final Scanner newScanner = new Scanner(
-                    scanner.id(),
-                    scanner.beacons(),
-                    location.f().orientation(),
-                    location.delta());
-            beacons.addAll(newScanner.absoluteBeacons().stream()
-                    .map(p -> p.translate(newScanner.position()))
-                    .toList());
-        }
-
-        return beacons.size();
-    }
-
-    private Optional<List<Bar>> recursiveBuild(Map<Foo, List<Bar>> scannerMap, int expectedSize, List<Bar> candidates) {
-        List<Integer> candidateIds = candidates.stream().map(b -> b.f().id()).toList();
-        if (candidateIds.size() == expectedSize) {
-            System.out.println(candidates);
-            return Optional.of(candidates);
-        }
-
-        final List<Bar> subCandidates = new ArrayList<>();
-        for (var key : candidates) {
-            for (var value : scannerMap.get(key.f())) {
-                if (candidateIds.contains(value.f().id())) { continue; }
-                subCandidates.add(new Bar(value.f(), value.delta().translate(key.delta())));
-            }
-        }
-
-        for (Bar b : subCandidates) {
-            final var newCandidates = new ArrayList<>(candidates);
-            newCandidates.add(b);
-
-            Optional<List<Bar>> solution = recursiveBuild(scannerMap, expectedSize, newCandidates);
-            if (solution.isPresent()) { return solution; }
-        }
-
-        return Optional.empty();
-    }
-
-    public long oldSolve() {
         Set<Scanner> scanners = null;
 
+        // Pick ID0 as the root of the search space. We *should* be able to pick the default orientation and have
+        // everything else be relative to that, so this is probably unnecessary, but iterate over all possible
+        // orientations of the root note just in case. (And short-circuit if one matches.)
         boolean matched = false;
         int orientation = 0;
         while (!matched) {
@@ -126,16 +36,19 @@ public class Part1 {
                 return -1L;
             }
 
+            // Package the list of lists of beacons into a set of Scanner objects
             scanners = IntStream.range(0, provider.getReadouts().size())
                     .mapToObj(id -> new Scanner(id, provider.getReadouts().get(id)))
                     .collect(Collectors.toSet());
 
+            // Give the unoriented ID0 Scanner an orientation
             final Scanner first = scanners.stream()
                     .filter(s -> s.id() == 0)
                     .findFirst().orElseThrow();
             scanners.remove(first);
             scanners.add(new Scanner(first.id(), first.beacons(), orientation, new Point(0,0,0)));
 
+            // Attempt to match the rest of the scanners. This *should* work the first time.
             matched = locateScanners(scanners);
 
             orientation++;
@@ -145,12 +58,34 @@ public class Part1 {
                 .sorted(Comparator.comparing(Scanner::id))
                 .forEach(System.out::println);
 
-        final Set<Point> beacons = scanners.stream()
-                .flatMap(scanner -> scanner.absoluteBeacons().stream()
-                        .map(p -> p.translate(scanner.position())))
-                .collect(Collectors.toSet());
+        if (PART1) {
+            // For each scanner, translate its beacons from relative coordinates to absolute (relative to ID0).
+            // File the points into a Set to remove duplicates.
+            final Set<Point> beacons = scanners.stream()
+                    .flatMap(scanner -> scanner.absoluteBeacons().stream()
+                            .map(p -> p.translate(scanner.position())))
+                    .collect(Collectors.toSet());
 
-        return beacons.size();
+            return beacons.size();
+        } else {
+            // Calculate the Manhattan distance (dx+dy+dz) between each pair of scanners, and return the maximum.
+            // (Cut the diagonal on the search space.)
+            long farthestDistance = 0;
+
+            for (Scanner a : scanners) {
+                for (Scanner b : scanners) {
+                    if (a.id() >= b.id()) { continue; }
+                    final long distance = Math.abs(a.position().x() - b.position().x()) +
+                            Math.abs(a.position().y() - b.position().y()) +
+                            Math.abs(a.position().z() - b.position().z());
+                    if (distance > farthestDistance) {
+                        farthestDistance = distance;
+                    }
+                }
+            }
+
+            return farthestDistance;
+        }
     }
 
     private boolean locateScanners(Set<Scanner> scanners) {
